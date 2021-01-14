13813c1e444c39b70fa4b21a1d73eb540e124dc8
dominikus1993 <dominikus1910@gmail.com>
2021-01-14 20:17:44 +0100

    add frontent

--- web/src/App.js
+++ web/src/App.js
@@ -1,78 +1,74 @@
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
+import axios from "axios"
 const ENDPOINT = "http://127.0.0.1:4001";
 
-function list(data) {
+
+/**
+ * @param {{ url: string, content: {url: string}[] }} props
+ */
+function ContentList(props) {
   return (
     <div>
-      <p>Url: {data.url}</p>
+      <p>Url: {props.url}</p>
       <ul>
-        {data.contents.map(content => (
+        {props.contents.map(content => (
           <li key={content.url}>
             <img src={content.url} />
           </li>
         ))}
       </ul>
-    </div> 
+    </div>
   );
 }
 
-function form() {
-  const [url, setUrl] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
- 
+function ParseUrlForm() {
+  const [url, setUrl] = useState("");
+  const [query, setQuery] = useState("")
+
   useEffect(() => {
     const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
- 
-      try {
-        const result = await axios(url);
- 
-        setData(result.data);
-      } catch (error) {
-        setIsError(true);
+      if (url !== "") {
+        console.log("Request");
+        await axios.post(ENDPOINT, { url: url });
       }
- 
-      setIsLoading(false);
+
     };
- 
     fetchData();
   }, [url]);
   return <form>
-          <input
-        type="text"
-        value={query}
-        onChange={event => setQuery(event.target.value)}
-      />
-      <button
-        type="button"
-        onClick={() =>
-          setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
-        }
-      >
-        Search
+    <input
+      type="text"
+      value={query}
+      onChange={event => setQuery(event.target.value)}
+    />
+    <button
+      type="button"
+      onClick={() =>
+        setUrl(query)
+      }
+    >
+      Search
       </button>
   </form>
 }
 
 function App() {
-  const [response, setResponse] = useState({hasData: false, url: "", contents: [] });
-  
+  const [response, setResponse] = useState({ hasData: false, url: "", contents: [] });
+
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
     socket.on("new-crawled-media", data => {
-      setResponse({hasData: true, url: data.url, contents: data.contents });
+      setResponse({ hasData: true, url: data.url, contents: data.contents });
     });
     return () => socket.disconnect()
   }, []);
 
-  if(response.hasData) {
-    return list(response)
+  if (response.hasData) {
+    return <ContentList url={response.url} contents={response.contents} ></ContentList>
   }
   else {
-    return <h1>Witam</h1>
+    return <ParseUrlForm></ParseUrlForm>
   }
 }
 
