941f510420d66d62b0d21ed816aa2f38e61ca1a4
dominikus1993 <dominikus1910@gmail.com>
2021-03-16 19:34:16 +0100

    remove code

--- crawler/go.mod
+++ crawler/go.mod
@@ -3,6 +3,7 @@ module crawler
 go 1.16
 
 require (
+	github.com/dapr/go-sdk v1.0.0 // indirect
 	github.com/gocolly/colly/v2 v2.1.0
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/streadway/amqp v1.0.0
--- crawler/main.go
+++ crawler/main.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"context"
-	"crawler/application/usecase"
-	"crawler/infrastructure/env"
-	"crawler/infrastructure/service"
-
 	log "github.com/sirupsen/logrus"
-	"github.com/streadway/amqp"
 )
 
 func createLogger() *log.Logger {
@@ -16,24 +10,11 @@ func createLogger() *log.Logger {
 		// disable, as we set our own
 		DisableTimestamp: true,
 	}
-
 	return logger
 }
 
 func main() {
 	logger := createLogger()
-	log.Infoln("Start Service")
-	connection := env.GetEnvOrDefault("RABBITMQ_CONNECTION", "amqp://guest:guest@localhost:5672/")
-	conn, err := amqp.Dial(connection)
-	if err != nil {
-		log.WithError(err).Fatalln("Failed to connect to RabbitMQ")
-	}
-	defer conn.Close()
-	parser := service.NewWebsiteParser()
-	publisher := service.NewMessagePublisher(conn)
-	consumer := service.NewMessageConsumer(conn, logger)
-	usecase := usecase.NewCrawlerUseCase(parser, publisher, consumer)
-	ctx := context.TODO()
-	usecase.StartCrawling(ctx)
-	log.Infoln("Stop Service")
+	logger.Infoln("Start Service")
+
 }
