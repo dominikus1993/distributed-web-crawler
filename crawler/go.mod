a00eea14f4e03ec46b36999d97f70e663ea50c48
dominikus1993 <dominikus1910@gmail.com>
2021-03-17 19:53:51 +0100

    add simple publisher

--- crawler/go.mod
+++ crawler/go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/dapr/go-sdk v1.0.0 // indirect
 	github.com/gocolly/colly/v2 v2.1.0
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/streadway/amqp v1.0.0
 )
--- crawler/main.go
+++ crawler/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"crawler/domain/model"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
 	dapr "github.com/dapr/go-sdk/client"
+	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -14,6 +21,25 @@ func createLogger() *log.Logger {
 	return logger
 }
 
+func hello(d dapr.Client) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		resp := model.CrawledWebsite{Url: "tets"}
+		res, err := json.Marshal(resp)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintln(w, "Error")
+		}
+		err = d.PublishEvent(r.Context(), "pubsub", "test", res)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintln(w, "Error dapr", err)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "Welcome!")
+	}
+}
+
 func main() {
 	logger := createLogger()
 	logger.Infoln("Start Service")
@@ -22,4 +48,16 @@ func main() {
 		panic(err)
 	}
 	defer client.Close()
+	router := mux.NewRouter()
+	router.HandleFunc("/hello", hello(client))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    "127.0.0.1:5000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
